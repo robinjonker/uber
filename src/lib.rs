a2a3134538cc40d01573a58a080c67583b042091
Robin Jonker <87306145+robinjonker@users.noreply.github.com>
2023-03-14 16:12:07 +0200

    rem content_type

--- src/lib.rs
+++ src/lib.rs
@@ -784,13 +784,13 @@ pub async fn cancel_delivery(
     //     delivery_id: delivery_id.to_string()
     // };
 
-    let content_type = HeaderValue::from_str("application/json")?;
+    // let content_type = HeaderValue::from_str("application/json")?;
     let auth_header = format!("Bearer {}", access_token);
     let authorization = HeaderValue::from_str(&auth_header)?;
     // let body = serde_json::to_string(&cancel_delivery_request)?;
 
     let res = client.post(&url)
-        .header(CONTENT_TYPE, content_type)
+        // .header(CONTENT_TYPE, content_type)
         .header(AUTHORIZATION, authorization)
         // .body(body)
         .send()
